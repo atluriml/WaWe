b17c45546089a0717b4a18d8b4e1a605c05518c6
Megana Atluri <megana@fb.com>
2022-06-22 11:02:08 -0700

    updated project by adding different search methods to the RestaurantClient

--- app/src/main/java/com/example/wawe/RestaurantClient.java
+++ app/src/main/java/com/example/wawe/RestaurantClient.java
@@ -10,11 +10,17 @@ import retrofit2.http.Query;
 
 public interface RestaurantClient  {
 
+    @GET("businesses/categories")
+    Call<RestaurantSearch> getCategories(@Header("Authorization") String authToken, @Query("categories") String categories);
 
-    @GET("authorize")
-    Call<ResponseBody> getSecret(@Header("Authorization") String authToken);
-
+    // if the user only uses one term and one location
     @GET("businesses/search")
-    Call<ResponseBody> searchRestaurants(@Header("Authorization") String authToken, @Query("term") String keyword, @Query("location") String location);
+    Call<RestaurantSearch> searchRestaurants(@Header("Authorization") String authToken, @Query("categories") String cuisine, @Query("categories") String dietaryRestriction, @Query("location") String location, @Query("price") String price);
+
+    // if the user uses all of the filters
+    @GET("businesses/search")
+    Call<RestaurantSearch> searchRestaurants(@Header("Authorization") String authToken, @Query("categories") String cuisine, @Query("location") String location, @Query("price") String price);
+
+
 
 }
