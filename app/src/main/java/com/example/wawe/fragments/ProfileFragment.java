6f7705f0a14733409611de9be1623937a736ecec
Megana Atluri <megana@fb.com>
2022-07-15 11:34:27 -0700

    updated ui for splash screen, profile fragment, roulette fragment, and restaurantLists

--- app/src/main/java/com/example/wawe/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/wawe/fragments/ProfileFragment.java
@@ -1,21 +1,10 @@
 package com.example.wawe.fragments;
 
-import static android.app.Activity.RESULT_OK;
-
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
-
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,19 +12,12 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
 import com.example.wawe.Activities.RestaurantListsActivity;
 import com.example.wawe.Activities.SettingsActivity;
 import com.example.wawe.R;
-import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 
 /**
  * A simple {@link Fragment} subclass.
