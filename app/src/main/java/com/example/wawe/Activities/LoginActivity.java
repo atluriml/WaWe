629d383445990192424ac81d619573e051172362
Megana Atluri <megana@fb.com>
2022-07-21 11:16:49 -0700

    updated signup and login to implement more error handling

--- app/src/main/java/com/example/wawe/Activities/LoginActivity.java
+++ app/src/main/java/com/example/wawe/Activities/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.wawe.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etPassword;
     private Button btnLogin;
     private Button btnSignUp;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +32,17 @@ public class LoginActivity extends AppCompatActivity {
             goMainActivity();
         }
 
+        loadingBar = new ProgressDialog(this);
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (!MainActivity.isOnline(LoginActivity.this)){
+                    Toast.makeText(LoginActivity.this, "Must be connected to the internet to login", Toast.LENGTH_SHORT).show();
+                    setLoginCredentialsToNull();
+                }
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
@@ -55,15 +63,31 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUser(String username, String password){
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                if(e != null){
-                    return;
+        if (TextUtils.isEmpty(etUsername.getText().toString())){
+            Toast.makeText(LoginActivity.this, "Please enter username", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(etPassword.getText().toString())){
+            Toast.makeText(LoginActivity.this, "Please enter a password", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loadingBar.setTitle("Logging In");
+            loadingBar.setMessage("Please wait, while we are logging you in");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+            ParseUser.logInInBackground(username, password, new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (e != null) {
+                        Toast.makeText(LoginActivity.this, "Error Logging in. Make sure your credentials are correct", Toast.LENGTH_SHORT).show();
+                        setLoginCredentialsToNull();
+                        loadingBar.dismiss();
+                        return;
+                    }
+                    goMainActivity();
+                    loadingBar.dismiss();
                 }
-                goMainActivity();
-            }
-        });
+            });
+        }
     }
 
     // when the user successfully logs in the program then goes to the MainActivity
@@ -71,5 +95,10 @@ public class LoginActivity extends AppCompatActivity {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
+    }
+
+    private void setLoginCredentialsToNull () {
+        etPassword.setText(null);
+        etUsername.setText(null);
     }
 }
--- app/src/main/java/com/example/wawe/Activities/SignUpActivity.java
+++ app/src/main/java/com/example/wawe/Activities/SignUpActivity.java
@@ -1,16 +1,16 @@
 package com.example.wawe.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
-
 import com.example.wawe.R;
 import com.parse.ParseException;
 import com.parse.ParseUser;
@@ -24,12 +24,14 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText etLocation;
     private Button btnSignUp;
     private Spinner spDietaryRestriction;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        loadingBar = new ProgressDialog(this);
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         etRetypePassword = findViewById(R.id.etRetypePassword);
@@ -42,34 +44,57 @@ public class SignUpActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (!etPassword.getText().toString().equals(etRetypePassword.getText().toString())){
-                    Toast.makeText(SignUpActivity.this, "Please make sure your passwords match", Toast.LENGTH_SHORT).show();
+                if (!MainActivity.isOnline(SignUpActivity.this)){
+                    Toast.makeText(SignUpActivity.this, "Must be connected to the internet to create account", Toast.LENGTH_SHORT).show();
+                    setSignUpCredentialsToNull();
                 }
-                else {
-                    ParseUser user = new ParseUser();
-                    user.setUsername(etUsername.getText().toString());
-                    user.setPassword(etPassword.getText().toString());
-                    user.put("location", etLocation.getText().toString());
-                    if (!spDietaryRestriction.getSelectedItem().toString().equals("")) {
-                        user.put("dietaryRestriction", spDietaryRestriction.getSelectedItem().toString());
-                    }
-                    else {
-                        user.put("dietaryRestriction", " ");
-                    }
-                    user.signUpInBackground(new SignUpCallback() {
-                        @Override
-                        public void done(ParseException e) {
-                            if (e != null) {
-                                return;
-                            }
-                            goLoginActivity();
-                        }
-                    });
-                }
+                signUpUser();
             }
         });
 
+    }
+
+    private void signUpUser() {
+        if (TextUtils.isEmpty(etUsername.getText().toString())){
+            Toast.makeText(SignUpActivity.this, "Please enter username", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(etPassword.getText().toString())){
+            Toast.makeText(SignUpActivity.this, "Please enter a password", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(etLocation.getText().toString())){
+            Toast.makeText(SignUpActivity.this, "Please enter a location", Toast.LENGTH_SHORT).show();
+        }
+        else if (!etPassword.getText().toString().equals(etRetypePassword.getText().toString())){
+            Toast.makeText(SignUpActivity.this, "Please make sure your passwords match", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loadingBar.setTitle("Creating Account");
+            loadingBar.setMessage("Please wait, while we are creating your account");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+            ParseUser user = new ParseUser();
+            user.setUsername(etUsername.getText().toString());
+            user.setPassword(etPassword.getText().toString());
+            user.put("location", etLocation.getText().toString());
+            if (!spDietaryRestriction.getSelectedItem().toString().equals("")) {
+                user.put("dietaryRestriction", spDietaryRestriction.getSelectedItem().toString());
+            }
+            else {
+                user.put("dietaryRestriction", " ");
+            }
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e != null) {
+                        Toast.makeText(SignUpActivity.this, "Error Signing in. Make sure your credentials are correct", Toast.LENGTH_SHORT).show();
+                        setSignUpCredentialsToNull();
+                        return;
+                    }
+                    goLoginActivity();
+                    loadingBar.dismiss();
+                }
+            });
+        }
     }
 
     // logs new user in
@@ -77,5 +102,13 @@ public class SignUpActivity extends AppCompatActivity {
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
         finish();
+    }
+
+    private void setSignUpCredentialsToNull() {
+        etLocation.setText(null);
+        etPassword.setText(null);
+        etRetypePassword.setText(null);
+        etUsername.setText(null);
+        spDietaryRestriction.setSelection(0);
     }
 }
